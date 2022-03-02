dc1f4c5a78c29a31cab51fd759a930a5e4ee93b1
9nine99 <you@example.com>
2022-03-02 15:25:51 +0100

    agents have a goal

--- agents.py
+++ agents.py
@@ -1,10 +1,22 @@
+import numpy as np
+import operator
+
 class AGENT():
     def __init__(self,pos ) -> None:
         self.pos = pos
 
     def findGoal(self):
+        higher_value = np.where(self.comfort == np.max(self.comfort))
+        if len(higher_value[0]) == 2:
+            self.goal = (higher_value[0][0],higher_value[1][0])
+        else :
+            distance = {} 
+            for i in range(0, len(higher_value[0])):
+                (x1,y2) = (higher_value[0][i],higher_value[1][i])
+                distance[x1,y2] = ((x1 - agent.posx)**2 + (y2 - agent.posy)**2)**0.5
+            self.goal = min(distance, key = distance.get)
+        return self.goal
 
-        self.goal = ...
 
     def findNewPos(self):
         self.newPos = ...
