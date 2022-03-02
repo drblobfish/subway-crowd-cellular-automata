35b6179767b76f828b73d09576e664a6c9b166ec
dr_blobfish <jules.herrmann@gmail.com>
2022-03-02 10:08:22 +0100

    added broad architecture and empty methods to our classes

--- agents.py
+++ agents.py
@@ -1,3 +1,11 @@
 class AGENT():
     def __init__(self,pos ) -> None:
         self.pos = pos
+
+    def findGoal(self):
+
+        self.goal = ...
+
+    def findNewPos(self):
+        self.newPos = ...
+        return self.newPos
--- model.py
+++ model.py
@@ -1,5 +1,7 @@
 import constant as c
+
 import numpy as np
+from collections import defaultdict
 
 from agents import AGENT
 from restCell import RESTCELL
@@ -13,3 +15,34 @@ class MODEL() :
         self.agents = []
         self.restCells = []
         self.comfort = np.zeros((self.n,self.m))
+        self.walls = np.zeros((self.n,self.m))
+    
+    def newStep(self):
+        self.comfort = self.computeComfortMat()
+
+        self.findGoalForEachAgent()
+
+        self.findNewPosForEachAgent()
+
+        self.solveConflict()
+    
+    def computeComfortMat(self) -> np.array :
+        ...
+        return 
+    
+    def findGoalForEachAgent(self) -> None :
+        for agent in self.agents:
+            agent.findGoal(self.comfort)
+    
+    def findNewPosForEachAgent(self) -> None:
+
+        self.conflict = defaultdict(lambda : [])
+        for agent in self.agents:
+            pos = agent.findNewPos(self.walls)
+            self.conflict[agent].append(agent)
+    
+    def solveConflict(self) -> None :
+        #For each conflict (elements of self.conflict that have a lenth > 1)
+        # Solve them by choosing a random agent
+        # and the revert the newPos of the other agents to the current position
+        pass
