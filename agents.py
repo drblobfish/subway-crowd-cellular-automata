6fd72621e4c62b4990638bf5f365e891ad40a2f0
kiara <kiara.mangin@cri-paris.org>
2022-03-03 18:29:14 +0100

    trying to fix agent_allowed_to_stand_up

--- agents.py
+++ agents.py
@@ -1,7 +1,6 @@
 import math
 import numpy as np
 import constant as c
-from restCell import RESTCELL
 
 class AGENT():
     def __init__(self,pos,model,name="" ) -> None:
@@ -11,7 +10,6 @@ class AGENT():
          
 
     def findGoal(self,comfort):
-        agent_allowed_to_stand_up= "yes"
         comfort = comfort.copy()
         for i in [-1,0,1] :
             for j in [-1,0,1]:
@@ -28,7 +26,7 @@ class AGENT():
                 (x1,y2) = (higher_value[0][i],higher_value[1][i])
                 distance[x1,y2] = self.dist((x1,y2))
             self.goal = min(distance, key = distance.get)
-        if agent_allowed_to_stand_up == "no" :
+        if self.model.agent_allowed_to_stand_up == False:
             for i in self.model.restCells : 
                 if self.pos == i.pos : 
                     self.goal = self.pos 
